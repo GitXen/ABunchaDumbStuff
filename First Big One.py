22799cdd5c0b0f5c3252fcb9d9c105d19e4eace2
Xen <98061554+GitXen@users.noreply.github.com>
2022-05-22 22:13:55 -0400

    Idk
    What was this? I have no clue, Python bored me at this point.

---
+++ First Big One.py
@@ -0,0 +1,37 @@
+
+# from ast import Return
+
+# def The_Big_One(i,o,p):
+#     Death = i * o
+#     Life = i ** o
+#     Cum = Life / Death
+#     Sex = Life + Death / Cum
+#     return Sex
+
+# Test_01 = The_Big_One(10,5,6)
+
+# Test_02 = The_Big_One(20,10,12)
+#########################################
+# PART 2
+# gay = 10
+# dick_size = 6
+# puss = dick_size * 4
+# dick = puss / 4
+# 
+# 
+# caca = False
+# if  puss < gay:
+        # caca == True
+# 
+# print(dick_size,dick,puss,caca)
+# 
+balls = 10
+foreskin = 8
+dick = balls 
+list_balls = [0,2,3,4,5,6,7,8,9,10]
+if balls == foreskin:
+    print("Oh no, your balls are your foreskin!")
+elif balls == dick:
+        print("Ah, Picasso!")
+for i in list_balls:
+    print("The total number is", i)
