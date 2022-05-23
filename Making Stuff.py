8f7086fe29ead78c60bbd420e45cca921a9e3571
Xen <98061554+GitXen@users.noreply.github.com>
2022-05-22 22:15:17 -0400

    Haha Funny Jepstein
    Idk about this one either, Python rotted my brain

---
+++ Making Stuff.py
@@ -0,0 +1,22 @@
+import random
+question = "Did Jefferey Epistein Kill Himself?"
+
+
+if question == "Did Jefferey Epistein Kill Himself?":
+    print("No, now take your NORMAL PILLS FUCKER!!!!!")
+episode = random.randint(1,600)
+One_Piece_Good = False
+print("episode number",episode)
+if One_Piece_Good == True:
+    print("NO WRONG WRONG WRONG WRONG WRONG")
+elif One_Piece_Good == False:
+    print("Ah yes, a functioning humanbeing!")
+else:
+    print('UNG,BUNG,OOOG, DOOOOOOOG')
+#######################################################
+name = "Liam"
+age = 19
+day = "Saturday"
+message = ("How is your"+ " " + day + " "+ "going," + name + "?")
+print(message)
+input(AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH)
